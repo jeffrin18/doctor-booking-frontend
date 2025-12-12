6ea279e37589d69050735079bae2d59cead8f5d0
jeffrin18 <jeffrinstewartb@gmail.com>
2025-12-12 10:19:59 +0530

    Fix typescript build errors

--- src/context/BookingContext.tsx
+++ src/context/BookingContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, ReactNode } from 'react';
+import { createContext, useContext, useState, type ReactNode } from 'react';
 
 // Define what our data looks like
 interface Doctor {
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import apiClient from '../api/client';
 import { useBooking } from '../context/BookingContext';
